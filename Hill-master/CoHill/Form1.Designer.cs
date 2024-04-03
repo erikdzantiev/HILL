b1da724a4648ca14cfd593e7638c107bedcb9487
erikdzantiev <81524099+erikdzantiev@users.noreply.github.com>
2024-04-03 18:29:15 +0300

    Add files via upload

---
+++ Hill-master/CoHill/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace CoHill
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewMain = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelS2 = new System.Windows.Forms.Label();
+            this.labelS1 = new System.Windows.Forms.Label();
+            this.textBoxSos2 = new System.Windows.Forms.TextBox();
+            this.textBoxSos1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewMain
+            // 
+            this.dataGridViewMain.AllowUserToAddRows = false;
+            this.dataGridViewMain.AllowUserToDeleteRows = false;
+            this.dataGridViewMain.AllowUserToResizeColumns = false;
+            this.dataGridViewMain.AllowUserToResizeRows = false;
+            this.dataGridViewMain.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewMain.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridViewMain.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridViewMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMain.ColumnHeadersVisible = false;
+            this.dataGridViewMain.Location = new System.Drawing.Point(43, 117);
+            this.dataGridViewMain.MultiSelect = false;
+            this.dataGridViewMain.Name = "dataGridViewMain";
+            this.dataGridViewMain.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridViewMain.RowHeadersVisible = false;
+            this.dataGridViewMain.RowHeadersWidth = 51;
+            this.dataGridViewMain.RowTemplate.Height = 50;
+            this.dataGridViewMain.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridViewMain.Size = new System.Drawing.Size(212, 194);
+            this.dataGridViewMain.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(43, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "зашифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(191, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(261, 409);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(146, 29);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(39, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите слово:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(33, 409);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Зашифрованное слово:";
+            // 
+            // labelS2
+            // 
+            this.labelS2.AutoSize = true;
+            this.labelS2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelS2.Location = new System.Drawing.Point(575, 161);
+            this.labelS2.Name = "labelS2";
+            this.labelS2.Size = new System.Drawing.Size(160, 24);
+            this.labelS2.TabIndex = 9;
+            this.labelS2.Text = "Расшифр. слово:";
+            // 
+            // labelS1
+            // 
+            this.labelS1.AutoSize = true;
+            this.labelS1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelS1.Location = new System.Drawing.Point(575, 18);
+            this.labelS1.Name = "labelS1";
+            this.labelS1.Size = new System.Drawing.Size(148, 24);
+            this.labelS1.TabIndex = 8;
+            this.labelS1.Text = "Введите шифр:";
+            // 
+            // textBoxSos2
+            // 
+            this.textBoxSos2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSos2.Location = new System.Drawing.Point(742, 158);
+            this.textBoxSos2.Name = "textBoxSos2";
+            this.textBoxSos2.Size = new System.Drawing.Size(146, 29);
+            this.textBoxSos2.TabIndex = 7;
+            // 
+            // textBoxSos1
+            // 
+            this.textBoxSos1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSos1.Location = new System.Drawing.Point(742, 18);
+            this.textBoxSos1.Name = "textBoxSos1";
+            this.textBoxSos1.Size = new System.Drawing.Size(146, 29);
+            this.textBoxSos1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(579, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(194, 37);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Расшифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(39, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 24);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Введите матрицу";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(900, 647);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.labelS2);
+            this.Controls.Add(this.labelS1);
+            this.Controls.Add(this.textBoxSos2);
+            this.Controls.Add(this.textBoxSos1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridViewMain);
+            this.Name = "Form1";
+            this.Text = "Шифр Хилла";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewMain;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelS2;
+        private System.Windows.Forms.Label labelS1;
+        private System.Windows.Forms.TextBox textBoxSos2;
+        private System.Windows.Forms.TextBox textBoxSos1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Hill-master/CoHill/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoHill
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<char, int> Alfavit = new Dictionary<char, int>();
+        public int[,] A;
+        public int[,] Z;
+        public string InpString;
+        public int n;
+        public int N = 33;
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridViewMain.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+            dataGridViewMain.Height = 180;
+            n = 3;
+            for (int i = 0; i < n; i++)
+            {
+                dataGridViewMain.Columns.Add("Column" + i.ToString(), "");
+                dataGridViewMain.Rows.Add();
+                dataGridViewMain.Rows[i].Height = dataGridViewMain.Height / n-1;
+            }
+            char[] alpha = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ".ToCharArray();
+            for (int i = 0; i < alpha.Length; i++)
+            {
+                Alfavit.Add(alpha[i], (i+1) % alpha.Length);
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetMassA();
+            GetInverseMatrix(A);
+            InpString = textBox1.Text;
+            while (InpString.Length % n != 0)
+            {
+                InpString += "Z";
+            }
+            InpString = InpString.ToUpper();
+            CreateHill1(A, InpString, textBox2);
+        }
+
+        private void CreateHill1(int[,] A, string s, TextBox textbox)
+        {
+            while(s.Length % n != 0)
+            {
+                s += "Z";
+            }
+            char[] beta = s.ToCharArray();
+            int t = s.Length / n;
+            int[][] mda = new int[t][];
+            for (int i = 0; i < t; i++)
+            {
+                mda[i] = new int[n];
+                for (int j = 0; j < n; j++)
+                {
+                    mda[i][j] = Alfavit[beta[i*n+j]];
+                }
+            }//составил вектора из буковок)
+
+            int[][] newmda = new int[t][];
+            for (int i = 0; i < t; i++)
+            {
+                newmda[i] = Multi(A, mda[i]);
+            }
+            string res = "";
+            for (int i = 0; i < t; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    res += Alfavit.FirstOrDefault(x => x.Value==newmda[i][j]).Key;
+                }
+            }
+            textbox.Text = res;
+        }
+
+        private int[] Multi(int[,] a, int[] b)
+        {
+            int[] res = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    sum += (a[i, j] * b[j]) % N;
+                }
+                res[i] = sum % N;
+            }
+            return res;
+        }
+
+        public void GetMassA()
+        {
+            A = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    A[i, j] = int.Parse(dataGridViewMain.Rows[i].Cells[j].Value.ToString());
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void GetInverseMatrix(int[,] matrix)
+        {
+            int[,] newMatrix;
+            int opr = GetOpr(matrix); //находим определитель
+            int invOpr = GetInverseOprforModule(opr, N); // обратное определителю по модулю
+            newMatrix = Transpose(GetInverseMatrix2(matrix, invOpr));
+            Z = newMatrix;
+        }
+
+        public int GetOpr(int[,] matrix)
+        {
+            return ((matrix[0, 0] * matrix[1, 1] * matrix[2, 2]
+                + matrix[1, 0] * matrix[2, 1] * matrix[0, 2]
+                + matrix[0, 1] * matrix[1, 2] * matrix[2, 0])
+                - (matrix[0, 2] * matrix[1, 1] * matrix[2, 0]
+                + matrix[0, 0] * matrix[1, 2] * matrix[2, 1]
+                + matrix[2, 2] * matrix[0, 1] * matrix[1, 0]) + N*100) % N;
+        }
+
+        public int GetInverseOprforModule(int opr, int mod)
+        {
+            for (int i = 0; i < mod; i++)
+            {
+                if ((opr * i) % mod == 1)
+                {
+                    return i;
+                }
+            }
+            return 1;
+        }
+
+        public int[,] GetInverseMatrix2(int[,] a, int opr)
+        {
+            var res = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    res[i, j] = (GetMinor(a, i, j)*opr*((i+j) % 2==0 ? 1:-1) + 100*N) % N;
+                }
+            }
+            return res;
+        }
+
+        public int GetMinor(int[,] mas, int a, int b)
+        {
+            List<int> newMatrix2x2 = new List<int>((n-1)*(n-1));
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != a && j !=b)
+                    {
+                        newMatrix2x2.Add(mas[i, j]);
+                    }
+                }
+            }
+            return (newMatrix2x2[0] * newMatrix2x2[3] - newMatrix2x2[1] * newMatrix2x2[2] + 100*N) % N;
+        }
+
+        public int[,] Transpose(int[,] a)
+        {
+            var b = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    b[i, j] = a[j, i];
+                }
+            }
+            return b;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            InpString = textBoxSos1.Text;
+            while (InpString.Length % n != 0)
+            {
+                InpString += "Z";
+            }
+            InpString = InpString.ToUpper();
+            CreateHill1(Z, InpString, textBoxSos2);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
